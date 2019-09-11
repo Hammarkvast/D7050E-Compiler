65e96adaa5ba4ae6485001fd5d7e65c606b82348
Hammarkvast <tomham-3@student.ltu.se>
2019-09-11 10:58:07 +0200

    Var parser working

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate nom;
 use nom::{
     branch::alt,
     IResult,
-    bytes::complete::{tag, take_while_m_n},
+    character::is_alphanumeric,
+    character::complete::alpha1,
+    bytes::complete::{tag, take_while_m_n, take_while},
     combinator::map,
     sequence::{tuple, preceded},
     character::complete::{digit1, multispace0}};
@@ -18,13 +20,37 @@ enum op {
 }
 
 #[derive(Debug)]
+enum types{
+    integer32,
+    boolean,
+}
+
+#[derive(Debug)]
+enum variable{
+    name,
+    value,
+}
+
+#[derive(Debug)]
 enum list {
     Cons(Box<list>, op, Box<list>),
+    VarBox(Box<list>, op, Box<list>),
     Num(i32),
+    Var(String),
 }
 
 fn parser(input:&str) -> IResult<&str, &str> {
     digit1(input)
+}
+
+fn parserTest(input:&str) -> IResult<&str, &str> {
+    preceded(
+        multispace0,
+        alt((
+            digit1,            
+            take_while(char::is_alphanumeric),
+    )),
+    )(input)
 }
 
 fn parseInput(input: &str) -> (&str, &str){
@@ -37,6 +63,21 @@ fn parseInput(input: &str) -> (&str, &str){
 }
 
 fn BoxInput(input: &str) ->Box<list>{
+     match parserTest(input){
+        Ok(k) => {
+            let isLetter: IResult<&str,&str> = alpha1(k.1);
+            match isLetter{
+                Ok(r) => {
+                    println!("{:?}", k.1);
+                    let theSign = getSign(k.0);
+                    let new_string = r.1.to_string();
+                    let VarTreelist = list::VarBox(Box::new(list::Var(new_string)), theSign.1, BoxInput(&k.0[1..]));
+                    return Box::new(VarTreelist);
+                }Err(t) => (),
+            }
+            }Err(e) => ()
+        ,
+    }
     let (signNum, numStr) = parseInput(input);
     let number = numStr.parse::<i32>().unwrap();
     let sign = getSign((signNum));
@@ -47,6 +88,8 @@ fn BoxInput(input: &str) ->Box<list>{
     let treeList = list::Cons(Box::new(list::Num(number)), sign.1, BoxInput(&signNum[1..]));
     return Box::new(treeList);
 }
+
+
 
 fn getSign(input: &str) ->(&str, op) {
     let sign: IResult<&str, op> = preceded(
@@ -65,6 +108,29 @@ fn getSign(input: &str) ->(&str, op) {
     }
 }
 
+//fn 
+
 fn main() {
-    println!("output: {:?}", BoxInput("20*2/2%6"));
+//    println!("output: {:?}", digit1("2"));
+    println!("{:?}", BoxInput("a+1+1"));
+
+//    let a: IResult<&str,&str> = alpha1("1+1");
+ //   println!("{:?}",a);
+//     match a{
+//         Ok(k) => {
+//             let isLetter: IResult<&str,&str> = alpha0(k.1);
+//             match isLetter{
+//                 Ok(r) => {
+//                  let theSign = getSign(k.0);
+//                  let new_string = r.1.to_string();
+// //                 let VarTreelist = list::VarBox(Box::new(list::Var(new_string)), theSign.1, BoxInput(&k.0[1..]));
+// //                 return Box::new(VarTreelist);
+//                  println!("{:?}", theSign.1);                  
+//                 }
+//                 Err(l) => (),
+//             }
+//             }
+//         ,
+//         Err(e) => (),
+//     }
 }
